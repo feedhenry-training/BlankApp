575b06a9d7e7915b1bac7204b12456846f020ac5
Wei Li <wei.li@feedhenry.com>
2013-09-12 15:45:52 +0100

    convert back to use fh-nodeapp

--- cloud/application.js
+++ cloud/application.js
@@ -1,20 +1,3 @@
-var express = require('express');
-
-//FeedHenry services
-var webapp = require('fh-webapp');
-$fh = require('fh-api');
-var mainjs = require('main.js');
-
-var app = express();
-
-//enable FeedHenry services
-app.use('/sys', webapp.sys(mainjs));
-app.use('/mbaas', webapp.mbaas);
-app.use('/cloud', webapp.cloud(mainjs));
-
-// You can define custom URL handlers here, like this one:
-app.use('/', function(req, res){
-  res.end('Your Cloud App is Ready');
-});
-
-module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
+var nodeapp = require("fh-nodeapp");
+nodeapp.HostApp.init();
+nodeapp.HostApp.serveApp(require('main.js'));
